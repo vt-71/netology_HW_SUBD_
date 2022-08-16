285ee6426fd725e79d918d58e5a4d63d9a8bca85
Vladimir Tyan <tyanv01@gmail.com>
2022-08-16 19:08:22 +0300

    Добавлена ДЗ 2 СУБД

---
+++ sql_create_BD.sql
@@ -0,0 +1,39 @@
+CREATE TABLE IF NOT EXISTS Genres (
+  id SERIAL PRIMARY KEY,
+  name_genre VARCHAR(60) UNIQUE NOT NULL);
+
+CREATE TABLE IF NOT EXISTS Artists (
+  id SERIAL PRIMARY KEY,
+  name_artist VARCHAR(20) UNIQUE NOT NULL);
+
+CREATE TABLE IF NOT EXISTS Genres_Artists (
+  artist_id INTEGER NOT NULL REFERENCES Artists (id),
+  genre_id INTEGER NOT NULL REFERENCES Genres (id),
+  CONSTRAINT pk PRIMARY KEY (artist_id, genre_id));
+
+CREATE TABLE IF NOT EXISTS Albums(
+  id SERIAL PRIMARY KEY,
+  name_album VARCHAR(60) UNIQUE NOT NULL,
+  year_album INTEGER NOT NULL,
+--  CHECK (YEAR > 1900),
+  artist VARCHAR(60) UNIQUE NOT NULL);
+
+CREATE TABLE IF NOT EXISTS Albums_Artists(
+  artist_id INTEGER NOT NULL REFERENCES Artists (id),
+  album_id INTEGER NOT NULL REFERENCES Albums (id),
+  CONSTRAINT pk_1 PRIMARY KEY (artist_id, album_id));
+
+CREATE TABLE IF NOT EXISTS Tracks(
+  id SERIAL PRIMARY KEY,
+  name_track VARCHAR(60) UNIQUE NOT NULL,
+  time_track TIME NOT NULL,
+  album_id INTEGER NOT NULL REFERENCES Albums (id));
+
+CREATE TABLE IF NOT EXISTS Compilations (
+  id SERIAL PRIMARY KEY,
+  name_compilation VARCHAR(60) UNIQUE NOT NULL,
+  year_compilation INTEGER NOT NULL,
+--  CHECK (year_compilation > 1900),
+  track_id INTEGER NOT NULL REFERENCES Tracks(id));
+
+
