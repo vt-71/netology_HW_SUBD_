46e0c034772ab38c97c842be942ce81e32e1f73d
vt-71 <tyanv01@gmail.com>
2022-08-31 15:54:45 +0300

    gitignore

--- sql_create_BD.sql
+++ sql_create_BD.sql
@@ -1,41 +1,40 @@
-CREATE TABLE IF NOT EXISTS Genres (
-  id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS genres (
+  genre_id SERIAL PRIMARY KEY,
   name_genre VARCHAR(60) UNIQUE NOT NULL);
 
-CREATE TABLE IF NOT EXISTS Artists (
-  id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS artists (
+  artist_id SERIAL PRIMARY KEY,
   name_artist VARCHAR(20) UNIQUE NOT NULL);
 
-CREATE TABLE IF NOT EXISTS Genres_Artists (
-  artist_id INTEGER NOT NULL REFERENCES Artists (id),
-  genre_id INTEGER NOT NULL REFERENCES Genres (id),
+CREATE TABLE IF NOT EXISTS styles (
+  artist_id INTEGER NOT NULL REFERENCES artists (artist_id),
+  genre_id INTEGER NOT NULL REFERENCES genres (genre_id),
   CONSTRAINT pk PRIMARY KEY (artist_id, genre_id));
 
-CREATE TABLE IF NOT EXISTS Albums(
-  id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS albums(
+  album_id SERIAL PRIMARY KEY,
   name_album VARCHAR(60) UNIQUE NOT NULL,
-  year_album INTEGER NOT NULL,
---  CHECK (YEAR > 1900),
-  artist VARCHAR(60) UNIQUE NOT NULL);
+  year_album INTEGER NOT NULL CHECK (year_album > 1900));
 
-CREATE TABLE IF NOT EXISTS Albums_Artists(
-  artist_id INTEGER NOT NULL REFERENCES Artists (id),
-  album_id INTEGER NOT NULL REFERENCES Albums (id),
+
+CREATE TABLE IF NOT EXISTS discographies(
+  artist_id INTEGER NOT NULL REFERENCES artists (artist_id),
+  album_id INTEGER NOT NULL REFERENCES albums (album_id),
   CONSTRAINT pk_1 PRIMARY KEY (artist_id, album_id));
 
-CREATE TABLE IF NOT EXISTS Tracks(
-  id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS tracks(
+  track_id SERIAL PRIMARY KEY,
   name_track VARCHAR(60) UNIQUE NOT NULL,
-  time_track TIME NOT NULL,
-  album_id INTEGER NOT NULL REFERENCES Albums (id));
+  time_track INTEGER NOT NULL,
+  album_id INTEGER NOT NULL REFERENCES albums (album_id));
 
-CREATE TABLE IF NOT EXISTS Compilations (
-  id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS compilations (
+  compilation_id SERIAL PRIMARY KEY,
   name_compilation VARCHAR(60) UNIQUE NOT NULL,
-  year_compilation INTEGER NOT NULL);
+  year_compilation INTEGER NOT NULL CHECK (year_compilation > 1900));
 
-CREATE TABLE IF NOT EXISTS Tracks_Compilations (
-  track_id INTEGER NOT NULL REFERENCES Tracks (id),
-  compilation_id INTEGER NOT NULL REFERENCES Compilations (id),
+CREATE TABLE IF NOT EXISTS collections (
+  track_id INTEGER NOT NULL REFERENCES tracks (track_id),
+  compilation_id INTEGER NOT NULL REFERENCES compilations (compilation_id),
   CONSTRAINT pk_2 PRIMARY KEY (track_id, compilation_id));
 
